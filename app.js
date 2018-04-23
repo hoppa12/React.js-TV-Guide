e93e3c60a722be1e4be7c90fd390ea66253ccd1f
hoppa12 <troutwaterworld@gmail.com>
2018-04-23 21:55:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,68 +4,79 @@ import "./App.css";
 import TimeLine from "./components/TimeLine";
 import timeLineSize from "./components/testTimeLineSize";
 import Channel from "./components/Channel";
-class App extends Component 
-{
-  constructor(props) 
-  {
+import Categories from "./components/Categories";
+class App extends Component {
+  constructor(props) {
     super(props);
-    (this.state = 
-    {
+    (this.state = {
       time: this.initTime(),
       timeLineSize: null,
       hasLoaded: false,
       timeLineHeight: null,
-      vertical:0,
-      horizontal:0,
-      
+      vertical: 0,
+      horizontal: 0,
+      menuDisplayed: true,
+      min:0,
+      max:3
     }),
-      (this.globVars = 
-      {
-        currentChannel:null,
+      (this.globVars = {
+        channelPositon: null,
+        totalChannels:null,
+        currentChannel: null,
         hasLoaded: false,
-        TVEPG: null
+        TVEPG: null,
+        categories: [],
+        selectedNum: null
       });
   }
 
-  initTime() 
-  {
+  initTime() {
     return new Date(new Date().setHours(new Date().getHours(), 0, 0, 0));
   }
-  componentWillMount() 
-  {
+  componentWillMount() {
     this.getSkyJSON();
     document.addEventListener("keydown", this._handleKeyDown.bind(this));
   }
 
-  getSkyJSON() 
-  {
-    fetch("./TV.json")
-      .then(data => data.json())
+  getSkyJSON() {
+    let results = [];
+    results.push(
+      fetch("./TV.json")
+        .then(data => data.json())
 
-      .then(data => 
-      {
-        let res = data.channels.map(item => 
-        {
-          let adjShows = item.program.map(show => 
-            {
+        .then(data => {
+          let res = data.channels.map(item => {
+            let adjShows = item.program.map(show => {
               show.dur = parseInt(show.dur) * 1000;
               show.start = parseInt(show.start);
               return show;
             });
 
-          item.program = adjShows;
-          return item;
-        });
+            item.program = adjShows;
+            return item;
+          });
 
-        data.channels = res;
-        return data;
-      })
-      .then(data => (this.globVars.TVEPG = data))
-      .then(() => this.setState({ hasLoaded: true }));
+          data.channels = res;
+          return data;
+        })
+        .then(data => (this.globVars.TVEPG = data))
+    );
+    results.push(
+      fetch("ntv.json")
+        .then(data => data.json())
+        .then(data => (this.globVars.ntv = data))
+        .then(() => {
+          this.globVars.categories = [
+            ...new Set(this.globVars.ntv.contents.map(item => item.genre))
+          ];
+        })
+    );
+
+    Promise.all(results).then(data => this.setState({ hasLoaded: true }));
   }
 
-  getCurrentShows(obj)
-  {
+  getCurrentShows(obj, vertical) {
+    console.log(obj.title);
     let res = JSON.parse(JSON.stringify(obj.program))
       .filter(
         item =>
@@ -79,48 +90,41 @@ class App extends Component
         const dur = item.dur;
         const epgStart = this.state.time.getTime() - 3600000;
         const epgEnd = this.state.time.getTime() + 3600000 * 3;
-        if (start < epgStart && start + dur > epgStart) 
-        {
+        if (start < epgStart && start + dur > epgStart) {
           const durationComplete = start + dur - epgStart;
 
           item.start = this.state.time.getTime();
           item.dur = durationComplete;
         }
 
-        if (start + dur > epgEnd) 
-        {
+        if (start + dur > epgEnd) {
           const durationEnd = epgEnd - start;
           item.dur = durationEnd;
         }
         return item;
-      })
+      });
 
+    if (vertical) {
       this.globVars.currentChannel = res;
+    }
 
-      return res;
-
-     
+    return res;
   }
 
-  componentDidMount() 
-  {
-    if (!this.state.timeLineSize) 
-    {
+  componentDidMount() {
+    if (!this.state.timeLineSize) {
       const itemWidth = document.querySelector(".hour").clientWidth;
       const itemHeight = document.querySelector(".content").clientHeight;
 
       this.setState({ timeLineSize: itemWidth, timeLineHeight: itemHeight });
     }
   }
- 
-  getTime() 
-  {
+
+  getTime() {
     let currentHour = this.state.time;
-    const hourString = hour =>
-    hour < 10 ? `0${hour}:00` : `${hour}:00`;
+    const hourString = hour => (hour < 10 ? `0${hour}:00` : `${hour}:00`);
     let arr = [];
-    for (let x = -1; x < 3; x++) 
-    {
+    for (let x = -1; x < 3; x++) {
       let hourTime = new Date(currentHour.getTime() + 3600000 * x);
       let res = hourString(new Date(hourTime).getHours());
       arr.push(res);
@@ -129,77 +133,170 @@ class App extends Component
     return arr;
   }
 
-  _handleKeyDown(e) 
-  {
+  handleCategories(e) {
+    console.log(e);
+  }
+  _handleKeyDown(e) {
     console.log(this.globVars.TVEPG);
     let myTime;
     let newHorizontal;
     let newVertical;
-    switch (e.keyCode) 
-    {
+    switch (e.keyCode) {
       case 39:
-      //  myTime = new Date(this.state.time.getTime() + 3600000);
-       // this.setState({ time: myTime });
-       if(this.state.horizontal === this.globVars.currentChannel.length-1)
-       {
-        myTime = new Date(this.state.time.getTime() + 3600000);
-        
-       
-         this.setState({ time: myTime });
-         if(this.globVars.currentChannel.length <=this.state.horizontal )
-         {
-           let horiz = this.globVars.currentChannel.length-1
-          this.setState({ horizontal: horiz });
-         }
-         break;
-       }
-       newHorizontal = this.state.horizontal+1;
-       this.setState({ horizontal: newHorizontal });
-      
+        //  myTime = new Date(this.state.time.getTime() + 3600000);
+        // this.setState({ time: myTime });
+
+        if (this.state.horizontal === this.globVars.currentChannel.length - 1) {
+          myTime = new Date(this.state.time.getTime() + 3600000);
+
+         
+          if (this.globVars.currentChannel.length <= this.state.horizontal) {
+            let horiz = this.globVars.currentChannel.length - 1;
+            this.setState({ horizontal: horiz,time: myTime  });
+            break;
+          }
+
+          this.setState({ time: myTime });
+          break;
+        }
+
+        newHorizontal = this.state.horizontal + 1;
+        this.setState({ horizontal: newHorizontal });
+
         break;
       case 37:
-       // myTime = new Date(this.state.time.getTime() - 3600000);
-      //  this.setState({ time: myTime });
+        if (
+          this.state.horizontal === 0 &&
+          this.globVars.currentChannel.length > 0
+        ) {
+          myTime = new Date(this.state.time.getTime() - 3600000);
 
-      if(this.state.horizontal === 0)
-      {
-        myTime = new Date(this.state.time.getTime() - 3600000);
-        
+          this.setState({ time: myTime });
+          break;
+        }
+        if (!this.globVars.currentChannel.length) 
+        {
+          myTime = new Date(this.state.time.getTime() + 3600000);
+
+          this.setState({ time: myTime });
+          if (this.globVars.currentChannel.length <= this.state.horizontal) {
+            let horiz = this.globVars.currentChannel.length - 1;
+            this.setState({ horizontal: horiz });
+          }
+          break;
+        }
+        newHorizontal = this.state.horizontal - 1;
+        this.setState({ horizontal: newHorizontal });
+        break;
+      case 38:
+        //up
+        this.globVars.channelPositon -= 1;
+        console.log(this.globVars.channelPositon);
+        let newPos = this.state.vertical - 1;
+        if (this.globVars.channelPositon <= 0 && this.state.menuDisplayed) {
+          this.setState({ vertical: 0 });
+          break;
+        }
+
+        if (newPos < 0 && !this.state.menuDisplayed) {
+          this.setState({ vertical: 0, menuDisplayed: true });
+          break;
+        }
+        newVertical = this.state.vertical - 1;
+        this.setState({ vertical: newVertical, horizontal: 0 });
+        break;
+
+      case 40:
+        //down
+        if (
+          this.state.vertical >= this.globVars.categories.length - 1 &&
+          this.state.menuDisplayed
+        ) {
+          this.setState({ vertical: this.globVars.categories.length - 1 });
+          break;
+        }
+
+        this.globVars.channelPositon += 1;
+        console.log(this.globVars.channelPositon )
+        if(this.globVars.channelPositon >= this.globVars.totalChannels&&
+          !this.state.menuDisplayed)
+        {
+          let res = this.state.min +1
+          newVertical = this.state.vertical + 1;
+          this.globVars.channelPositon -= 1;
+        this.setState({  horizontal: 0,min:res });
+        break;
+        }
        
-         this.setState({ time: myTime });
-        break;
-      }
-      newHorizontal = this.state.horizontal-1;
-       this.setState({ horizontal: newHorizontal });
-        break;
-        case 38:
-          //up
-          newVertical = this.state.vertical -1
-          this.setState({vertical:newVertical,horizontal:0});
-          break;
+        newVertical = this.state.vertical + 1;
+        this.setState({ vertical: newVertical, horizontal: 0 });
 
-        case 40:
-          //down
-          newVertical = this.state.vertical +1
-          this.setState({vertical:newVertical,horizontal:0});
-          break;
+        break;
+      case 13:
+        if (this.state.menuDisplayed) 
+        {
+          this.globVars.selectedNum = this.state.vertical;
+          this.globVars.channelPositon = 0;
+
+          this.setState({ vertical: 0, menuDisplayed: false });
+        }
+        break;
       default:
         break;
     }
-    
   }
 
-  render() 
+  getChannelLength()
   {
     
-    if (this.state.timeLineSize && this.state.hasLoaded) 
-    {
+    let res = this.globVars.TVEPG.channels
+                .filter(
+                  (item, index) =>
+                  this.state.min === index ||
+                    (this.state.min + 5 >= index
+                    && index > this.state.min))
+                this.globVars.totalChannels = res.length
+                console.log(res)
+    return res
+  }
+
+  render() {
+    let days = ["Mon", "Tue", "Wed", "Thurs", "Fri", "Sat", "Sun"];
+    let months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "June",
+      "July",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ];
+
+    if (this.state.hasLoaded && this.state.menuDisplayed) {
+      return (
+        <Categories
+          categories={this.globVars.categories}
+          vertical={this.state.vertical}
+        />
+      );
+    }
+    if (this.state.timeLineSize && this.state.hasLoaded) {
       return (
         <div className="App">
           <div className="mainGrid">
             <div className="content">
               <div className="timeline">
-                <div className="left"> </div>
+                <div className="left">
+                  <b>
+                    {days[this.state.time.getDay()]} {this.state.time.getDate()}{" "}
+                    {months[this.state.time.getMonth()]}
+                  </b>{" "}
+                </div>
                 <div className="bar">
                   {TimeLine(
                     this.getTime(),
@@ -209,18 +306,19 @@ class App extends Component
                 </div>
                 <div className="spacing" />
               </div>
-              {this.globVars.TVEPG.channels
-                .filter((item, index) => index < 7)
-                .map((channel,index) => (
-                  
+              {this.getChannelLength()
+                .map((channel, index) => (
                   <Channel
-                    obj={this.getCurrentShows(channel)}
+                    obj={this.getCurrentShows(
+                      channel,
+                      index === this.state.vertical
+                    )}
                     hourPx={this.state.timeLineSize}
                     height={this.state.timeLineHeight}
                     horizontal={this.state.horizontal}
                     timeState={this.state.time}
                     title={channel.title}
-                    vertical = {index === this.state.vertical}
+                    vertical={index === this.state.vertical}
                   />
                 ))}
             </div>
@@ -228,8 +326,7 @@ class App extends Component
           </div>
         </div>
       );
-    } else 
-    {
+    } else {
       return (
         <div className="App">
           <div className="mainGrid">
